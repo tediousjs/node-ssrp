473d20c966099a2ea13fc05d1680e6c60181c464
Arthur Schreiber <schreiber.arthur@gmail.com>
2017-03-23 22:23:52 +0100

    test: Test against both udp4 and udp6.

--- test/client-test.js
+++ test/client-test.js
@@ -8,215 +8,228 @@ const Instance = require('../src/instance');
 const Parser = require('../src/parser');
 
 describe('Client', function() {
-  beforeEach(function(done) {
-    this.server = dgram.createSocket('udp4');
+  ['udp4', 'udp6'].forEach(function(family) {
+    describe(`via ${family}`, function() {
+      let server, client, address;
 
-    // Flow does not know that the port option is optional.
-    // See https://github.com/facebook/flow/pull/3490.
-    // $FlowFixMe
-    this.server.bind(() => { done(); });
-  });
+      beforeEach(function(done) {
+        server = dgram.createSocket(family);
 
-  beforeEach(function() {
-    this.client = new Client();
-    this.client.port = this.server.address().port;
-    this.client.timeout = 100;
-  });
+        // Flow does not know that the port option is optional.
+        // See https://github.com/facebook/flow/pull/3490.
+        // $FlowFixMe
+        server.bind(() => { done(); });
+      });
 
-  describe('#listInstance', function(done) {
-    describe('with a valid response', function() {
       beforeEach(function() {
-        this.server.once('message', (data, address) => {
-          const response = Buffer.concat([
-            new Buffer([0x05, 0x58, 0x00]),
-            new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;IsClustered;No;Version;9.00.1399.06;tcp;57137;;')
-          ]);
-          this.server.send(response, 0, response.length, address.port, address.address);
-        });
+        address = {
+          address: family === 'udp6' ? '::1' : '127.0.0.1',
+          family: family === 'udp6' ? 6 : 4
+        };
       });
 
-      it('sends a CLNT_UCAST_INST request', function() {
-        this.server.once('message', (data) => {
-          const expected = Buffer.concat([
-            new Buffer([0x04]),
-            new Buffer('YUKONSTD')
-          ]);
-          assert.deepEqual(data, expected);
-        });
-
-        return this.client.listInstance(this.server.address(), 'YUKONSTD');
-      });
-
-      it('returns an Instance object', function() {
-        return this.client.listInstance(this.server.address(), 'YUKONSTD').then((instance) => {
-          assert.instanceOf(instance, Instance);
-
-          if (!instance) { return; }
-          assert.strictEqual(instance.serverName, 'ILSUNG1');
-          assert.strictEqual(instance.instanceName, 'YUKONSTD');
-          assert.isFalse(instance.isClustered);
-          assert.strictEqual(instance.version, '9.00.1399.06');
-          assert.strictEqual(instance.tcpPort, 57137);
-        });
-      });
-    });
-
-    describe('with an invalid response', function() {
-      it('raises a SyntaxError on invalid payload', function() {
-        this.server.once('message', (data, address) => {
-          const response = Buffer.concat([
-            new Buffer([0x05, 0x00, 0x40]),
-            new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;ThisWillNoLongerBeValid')
-          ]);
-          this.server.send(response, 0, response.length, address.port, address.address);
-        });
-
-        return this.client.listInstance(this.server.address(), 'YUKONSTD').then(() => {
-          assert.fail(true, false, 'Expected a SyntaxError, but none was thrown');
-        }, (err) => {
-          assert.instanceOf(err, Parser.SyntaxError);
-          assert.strictEqual(err.message, 'Expected "IsClustered" but "T" found.');
-        });
-      });
-
-      it('raises an Error on invalid response type', function() {
-        this.server.once('message', (data, address) => {
-          const response = new Buffer('This is a completely invalid response');
-
-          this.server.send(response, 0, response.length, address.port, address.address);
-        });
-
-        return this.client.listInstance(this.server.address(), 'YUKONSTD').then(() => {
-          assert.fail(true, false, 'Expected an Error, but none was thrown');
-        }, (err) => {
-          assert.instanceOf(err, Error);
-          assert.strictEqual(err.message, 'Invalid SSRP response.');
-        });
-      });
-    });
-
-    describe('without a response', function() {
-      it('times out after the specified timeout', function() {
-        const start = Date.now();
-
-        return this.client.listInstance(this.server.address(), 'YUKONSTD').then(() => {
-          const duration = Date.now() - start;
-
-          assert.isAtLeast(duration, this.client.timeout);
-          assert.isAtMost(duration, this.client.timeout + 100);
-        });
-      });
-
-      it('does not return an Instance object', function() {
-        return this.client.listInstance(this.server.address(), 'YUKONSTD').then((instance) => {
-          assert.equal(instance, undefined);
-        });
-      });
-    });
-  });
-
-  describe('#listInstances', function() {
-    describe('with a valid response', function() {
       beforeEach(function() {
-        this.server.once('message', (data, address) => {
-          const response = Buffer.concat([
-            new Buffer([0x05, 0x47, 0x01]),
-            new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;IsClustered;No;Version;9.00.1399.06;tcp;57137;;ServerName;ILSUNG1;InstanceName;YUKONDEV;IsClustered;No;Version;9.00.1399.06;np;\\\\ILSUNG1\\pipe\\MSSQL$YUKONDEV\\sql\\query;;ServerName;ILSUNG1;InstanceName;MSSQLSERVER;IsClustered;No;Version;9.00.1399.06;tcp;1433;np;\\\\ILSUNG1\\pipe\\sql\\query;;')
-          ]);
-          this.server.send(response, 0, response.length, address.port, address.address);
+        client = new Client();
+        client.port = server.address().port;
+        client.timeout = 100;
+      });
+
+      describe('#listInstance', function(done) {
+        describe('with a valid response', function() {
+          beforeEach(function() {
+            server.once('message', (data, address) => {
+              const response = Buffer.concat([
+                new Buffer([0x05, 0x58, 0x00]),
+                new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;IsClustered;No;Version;9.00.1399.06;tcp;57137;;')
+              ]);
+              server.send(response, 0, response.length, address.port, address.address);
+            });
+          });
+
+          it('sends a CLNT_UCAST_INST request', function() {
+            server.once('message', (data) => {
+              const expected = Buffer.concat([
+                new Buffer([0x04]),
+                new Buffer('YUKONSTD')
+              ]);
+              assert.deepEqual(data, expected);
+            });
+
+            return client.listInstance(address, 'YUKONSTD');
+          });
+
+          it('returns an Instance object', function() {
+            return client.listInstance(address, 'YUKONSTD').then((instance) => {
+              assert.instanceOf(instance, Instance);
+
+              if (!instance) { return; }
+              assert.strictEqual(instance.serverName, 'ILSUNG1');
+              assert.strictEqual(instance.instanceName, 'YUKONSTD');
+              assert.isFalse(instance.isClustered);
+              assert.strictEqual(instance.version, '9.00.1399.06');
+              assert.strictEqual(instance.tcpPort, 57137);
+            });
+          });
+        });
+
+        describe('with an invalid re ponse', function() {
+          it('raises a SyntaxError on invalid payload', function() {
+            server.once('message', (data, address) => {
+              const response = Buffer.concat([
+                new Buffer([0x05, 0x00, 0x40]),
+                new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;ThisWillNoLongerBeValid')
+              ]);
+              server.send(response, 0, response.length, address.port, address.address);
+            });
+
+            return client.listInstance(address, 'YUKONSTD').then(() => {
+              assert.fail(true, false, 'Expected a SyntaxError, but none was thrown');
+            }, (err) => {
+              assert.instanceOf(err, Parser.SyntaxError);
+              assert.strictEqual(err.message, 'Expected "IsClustered" but "T" found.');
+            });
+          });
+
+          it('raises an Error on invalid response type', function() {
+            server.once('message', (data, address) => {
+              const response = new Buffer('This is a completely invalid response');
+
+              server.send(response, 0, response.length, address.port, address.address);
+            });
+
+            return client.listInstance(address, 'YUKONSTD').then(() => {
+              assert.fail(true, false, 'Expected an Error, but none was thrown');
+            }, (err) => {
+              assert.instanceOf(err, Error);
+              assert.strictEqual(err.message, 'Invalid SSRP response.');
+            });
+          });
+        });
+
+        describe('without a response', function() {
+          it('times out after the specified timeout', function() {
+            const start = Date.now();
+
+            return client.listInstance(address, 'YUKONSTD').then(() => {
+              const duration = Date.now() - start;
+
+              assert.isAtLeast(duration, client.timeout);
+              assert.isAtMost(duration, client.timeout + 100);
+            });
+          });
+
+          it('does not return an Instance object', function() {
+            return client.listInstance(address, 'YUKONSTD').then((instance) => {
+              assert.equal(instance, undefined);
+            });
+          });
         });
       });
 
-      it('sends a CLNT_UCAST_EX request', function() {
-        this.server.once('message', (data) => {
-          const expected = new Buffer([0x03]);
-          assert.deepEqual(data, expected);
+      describe('#listInstances', function() {
+        describe('with a valid response', function() {
+          beforeEach(function() {
+            server.once('message', (data, address) => {
+              const response = Buffer.concat([
+                new Buffer([0x05, 0x47, 0x01]),
+                new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;IsClustered;No;Version;9.00.1399.06;tcp;57137;;ServerName;ILSUNG1;InstanceName;YUKONDEV;IsClustered;No;Version;9.00.1399.06;np;\\\\ILSUNG1\\pipe\\MSSQL$YUKONDEV\\sql\\query;;ServerName;ILSUNG1;InstanceName;MSSQLSERVER;IsClustered;No;Version;9.00.1399.06;tcp;1433;np;\\\\ILSUNG1\\pipe\\sql\\query;;')
+              ]);
+              server.send(response, 0, response.length, address.port, address.address);
+            });
+          });
+
+          it('sends a CLNT_UCAST_EX request', function() {
+            server.once('message', (data) => {
+              const expected = new Buffer([0x03]);
+              assert.deepEqual(data, expected);
+            });
+
+            return client.listInstances(address);
+          });
+
+          it('returns a list of Instance objects', function() {
+            return client.listInstances(address).then((instances) => {
+              let instance;
+              assert.lengthOf(instances, 3);
+
+              instance = instances[0];
+              assert.strictEqual(instance.serverName, 'ILSUNG1');
+              assert.strictEqual(instance.instanceName, 'YUKONSTD');
+              assert.isFalse(instance.isClustered);
+              assert.strictEqual(instance.version, '9.00.1399.06');
+              assert.strictEqual(instance.tcpPort, 57137);
+              assert.isUndefined(instance.npPipeName);
+
+              instance = instances[1];
+              assert.strictEqual(instance.serverName, 'ILSUNG1');
+              assert.strictEqual(instance.instanceName, 'YUKONDEV');
+              assert.isFalse(instance.isClustered);
+              assert.strictEqual(instance.version, '9.00.1399.06');
+              assert.isUndefined(instance.tcpPort);
+              assert.strictEqual(instance.npPipeName, '\\\\ILSUNG1\\pipe\\MSSQL$YUKONDEV\\sql\\query');
+
+              instance = instances[2];
+              assert.strictEqual(instance.serverName, 'ILSUNG1');
+              assert.strictEqual(instance.instanceName, 'MSSQLSERVER');
+              assert.isFalse(instance.isClustered);
+              assert.strictEqual(instance.version, '9.00.1399.06');
+              assert.strictEqual(instance.tcpPort, 1433);
+              assert.strictEqual(instance.npPipeName, '\\\\ILSUNG1\\pipe\\sql\\query');
+            });
+          });
         });
 
-        return this.client.listInstances(this.server.address());
-      });
+        describe('with an invalid response', function() {
+          it('raises a SyntaxError on invalid payload', function() {
+            server.once('message', (data, address) => {
+              const response = Buffer.concat([
+                new Buffer([0x05, 0x00, 0x40]),
+                new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;ThisWillNoLongerBeValid')
+              ]);
+              server.send(response, 0, response.length, address.port, address.address);
+            });
 
-      it('returns a list of Instance objects', function() {
-        return this.client.listInstances(this.server.address()).then((instances) => {
-          let instance;
-          assert.lengthOf(instances, 3);
+            return client.listInstances(address).then(() => {
+              assert.fail(true, false, 'Expected a SyntaxError, but none was thrown');
+            }, (err) => {
+              assert.instanceOf(err, Parser.SyntaxError);
+              assert.strictEqual(err.message, 'Expected "IsClustered" but "T" found.');
+            });
+          });
 
-          instance = instances[0];
-          assert.strictEqual(instance.serverName, 'ILSUNG1');
-          assert.strictEqual(instance.instanceName, 'YUKONSTD');
-          assert.isFalse(instance.isClustered);
-          assert.strictEqual(instance.version, '9.00.1399.06');
-          assert.strictEqual(instance.tcpPort, 57137);
-          assert.isUndefined(instance.npPipeName);
+          it('raises an Error on invalid response type', function() {
+            server.once('message', (data, address) => {
+              const response = new Buffer('This is a completely invalid response');
 
-          instance = instances[1];
-          assert.strictEqual(instance.serverName, 'ILSUNG1');
-          assert.strictEqual(instance.instanceName, 'YUKONDEV');
-          assert.isFalse(instance.isClustered);
-          assert.strictEqual(instance.version, '9.00.1399.06');
-          assert.isUndefined(instance.tcpPort);
-          assert.strictEqual(instance.npPipeName, '\\\\ILSUNG1\\pipe\\MSSQL$YUKONDEV\\sql\\query');
+              server.send(response, 0, response.length, address.port, address.address);
+            });
 
-          instance = instances[2];
-          assert.strictEqual(instance.serverName, 'ILSUNG1');
-          assert.strictEqual(instance.instanceName, 'MSSQLSERVER');
-          assert.isFalse(instance.isClustered);
-          assert.strictEqual(instance.version, '9.00.1399.06');
-          assert.strictEqual(instance.tcpPort, 1433);
-          assert.strictEqual(instance.npPipeName, '\\\\ILSUNG1\\pipe\\sql\\query');
-        });
-      });
-    });
-
-    describe('with an invalid response', function() {
-      it('raises a SyntaxError on invalid payload', function() {
-        this.server.once('message', (data, address) => {
-          const response = Buffer.concat([
-            new Buffer([0x05, 0x00, 0x40]),
-            new Buffer('ServerName;ILSUNG1;InstanceName;YUKONSTD;ThisWillNoLongerBeValid')
-          ]);
-          this.server.send(response, 0, response.length, address.port, address.address);
+            return client.listInstances(address).then(() => {
+              assert.fail(true, false, 'Expected an Error, but none was thrown');
+            }, (err) => {
+              assert.instanceOf(err, Error);
+              assert.strictEqual(err.message, 'Invalid SSRP response.');
+            });
+          });
         });
 
-        return this.client.listInstances(this.server.address()).then(() => {
-          assert.fail(true, false, 'Expected a SyntaxError, but none was thrown');
-        }, (err) => {
-          assert.instanceOf(err, Parser.SyntaxError);
-          assert.strictEqual(err.message, 'Expected "IsClustered" but "T" found.');
-        });
-      });
+        describe('without a response', function() {
+          it('times out after the specified timeout', function() {
+            const start = Date.now();
 
-      it('raises an Error on invalid response type', function() {
-        this.server.once('message', (data, address) => {
-          const response = new Buffer('This is a completely invalid response');
+            return client.listInstances(address).then(() => {
+              const duration = Date.now() - start;
 
-          this.server.send(response, 0, response.length, address.port, address.address);
-        });
+              assert.isAtLeast(duration, client.timeout);
+              assert.isAtMost(duration, client.timeout + 100);
+            });
+          });
 
-        return this.client.listInstances(this.server.address()).then(() => {
-          assert.fail(true, false, 'Expected an Error, but none was thrown');
-        }, (err) => {
-          assert.instanceOf(err, Error);
-          assert.strictEqual(err.message, 'Invalid SSRP response.');
-        });
-      });
-    });
-
-    describe('without a response', function() {
-      it('times out after the specified timeout', function() {
-        const start = Date.now();
-
-        return this.client.listInstances(this.server.address()).then(() => {
-          const duration = Date.now() - start;
-
-          assert.isAtLeast(duration, this.client.timeout);
-          assert.isAtMost(duration, this.client.timeout + 100);
-        });
-      });
-
-      it('returns an empty list', function() {
-        return this.client.listInstances(this.server.address()).then((instances) => {
-          assert.lengthOf(instances, 0);
+          it('returns an empty list', function() {
+            return client.listInstances(address).then((instances) => {
+              assert.lengthOf(instances, 0);
+            });
+          });
         });
       });
     });
